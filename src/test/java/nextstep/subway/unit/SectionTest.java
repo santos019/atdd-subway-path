91d24c0394cc82c07db23858c8a337c8b9412b36
santos019 <santos019@naver.com>
2024-07-27 03:29:54 +0900

    test: add SectionServiceTest class

--- src/test/java/nextstep/subway/unit/SectionTest.java
+++ src/test/java/nextstep/subway/unit/SectionTest.java
@@ -42,9 +42,10 @@ public class SectionTest {
     @Test
     @DisplayName("새로운 구간을 첫번째 구간에 추가한다.")
     public void add_section() {
-
+        // when
         생성된_구간들.addSection(역삼역_강남역);
 
+        // then
         Section 생성된_Section_조회 = 생성된_구간들.getSectionByUpStationId(역삼역.getId());
         Assertions.assertEquals(역삼역_강남역, 생성된_Section_조회);
     }
@@ -52,13 +53,15 @@ public class SectionTest {
     @Test
     @DisplayName("새로운 구간을 중간 구간에 추가한다.")
     public void add_section_middle() {
+        // given
         생성된_구간들.addSection(역삼역_강남역);
         생성된_구간들.addSection(강남역_선릉역);
 
+        // when
         Section 중간_구간에_추가할_Section = Section.of(강남역, 선정릉역, 1L);
         생성된_구간들.addSection(중간_구간에_추가할_Section);
 
-
+        // then
         Section 생성된_Section_조회 = 생성된_구간들.getSectionByUpStationId(강남역.getId());
         Assertions.assertEquals(중간_구간에_추가할_Section, 생성된_Section_조회);
     }
@@ -66,11 +69,14 @@ public class SectionTest {
     @Test
     @DisplayName("새로운 구간을 마지막 구간에 추가한다.")
     public void add_section_last() {
+        // given
         생성된_구간들.addSection(역삼역_강남역);
 
+        // when
         Section 마지막_구간에_추가할_Section = Section.of(강남역, 선릉역, 2L);
         생성된_구간들.addSection(마지막_구간에_추가할_Section);
 
+        // then
         Section 생성된_Section_조회 = 생성된_구간들.getSectionByUpStationId(강남역.getId());
         Assertions.assertEquals(마지막_구간에_추가할_Section, 생성된_Section_조회);
     }
@@ -78,8 +84,10 @@ public class SectionTest {
     @Test
     @DisplayName("새로운 구간의 상행역과 하행역 둘 다 기존 노선에 존재하면 구간 생성은 실패한다.")
     public void add_section_fail1() {
+        // given
         생성된_구간들.addSection(역삼역_강남역);
 
+        // when & then
         Assertions.assertThrows(SectionException.class, () -> 생성된_구간들.addSection(역삼역_강남역))
                 .getMessage().equals("새로운 구간의 상행역과 하행역이 이미 등록되어 있습니다.");
 
@@ -88,9 +96,11 @@ public class SectionTest {
     @Test
     @DisplayName("기존에 등록된 구간이 존재할 때, 새로운 구간의 상행역과 하행역 둘 다 기존 노선에 존재하지 않으면 구간 생성은 실패한다.")
     public void add_section_fail2() {
+        // given
         생성된_구간들.addSection(역삼역_강남역);
         Section 구간에_추가할_Section = Section.of(선정릉역, 선릉역, 2L);
 
+        // when & then
         Assertions.assertThrows(SectionException.class, () -> 생성된_구간들.addSection(구간에_추가할_Section))
                 .getMessage().equals("구간을 찾을 수 없습니다.");
     }
@@ -98,9 +108,11 @@ public class SectionTest {
     @Test
     @DisplayName("기존 구간의 Distance보다 길거나 같은 Distance를 가진 구간 생성은 실패한다.")
     public void add_section_fail3() {
+        // given
         생성된_구간들.addSection(역삼역_강남역);
         Section 구간에_추가할_Section = Section.of(역삼역, 선릉역, 2L);
 
+        // when & then
         Assertions.assertThrows(SectionException.class, () -> 생성된_구간들.addSection(구간에_추가할_Section))
                 .getMessage().equals("새로운 구간의 길이는 기존의 구간 길이보다 길어야합니다.");
 
