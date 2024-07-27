8273c745ea2f0f5a8b47183256ac84483d87efe1
santos019 <santos019@naver.com>
2024-07-28 01:54:00 +0900

    test: refactor StationAcceptanceTest class, clarify that acceptance tests are intended for stakeholders, not developers

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -1,78 +1,67 @@
 package nextstep.subway.station;
 
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import nextstep.subway.station.dto.StationResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static nextstep.subway.util.StationStep.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
 
-    List<String> STATION_NAME_LIST = List.of("강남역", "역삼역");
+    String 강남역 = "강남역";
+    String 역삼역 = "역삼역";
 
     @DisplayName("지하철역을 생성한다.")
     @Test
     void createStation() {
         // when
-        지하철_역_등록("강남역");
+        지하철_역_등록(강남역);
 
         // then
         List<String> stationNames = 지하철_역_전체_조회();
-        assertThat(stationNames).containsAnyOf("강남역");
+        assertThat(stationNames).containsAnyOf(강남역);
     }
 
     @DisplayName("2개의 지하철역을 생성한 다음, 2개의 지하철역을 조회한다.")
     @Test
     void showStation() {
         // given
-        for (int stationNameIdx = 0; stationNameIdx < STATION_NAME_LIST.size(); stationNameIdx++) {
-            String stationName = STATION_NAME_LIST.get(stationNameIdx);
-            지하철_역_등록(stationName);
-        }
+        지하철_역_등록(강남역);
+        지하철_역_등록(역삼역);
 
         // when
         List<String> stationNames = 지하철_역_전체_조회();
 
         // then
-        assertThat(stationNames.size()).isEqualTo(STATION_NAME_LIST.size());
-        assertThat(stationNames).containsAll(STATION_NAME_LIST);
+        assertThat(stationNames.size()).isEqualTo(2);
+        assertThat(stationNames).containsAll(List.of(강남역, 역삼역));
     }
 
     @DisplayName("지하철역을 생성한 다음, 해당 지하철역을 삭제한 뒤, 지하철역 목록을 조회하여 삭제된 것을 확인한다.")
     @Test
     void deleteStation() {
         // given
-        List<String> createdStationNameList = STATION_NAME_LIST;
-        List<Long> createdStationIdList = new ArrayList<>();
-
-        for (int stationNameIdx = 0; stationNameIdx < STATION_NAME_LIST.size(); stationNameIdx++) {
-            String stationName = STATION_NAME_LIST.get(stationNameIdx);
-            StationResponse createdStationResponse = 지하철_역_등록(stationName);
-            createdStationIdList.add(createdStationResponse.getId());
-        }
+        StationResponse 강남역_생성응답 = 지하철_역_등록(강남역);
+        StationResponse 역삼역_생성응답 = 지하철_역_등록(역삼역);
 
         List<String> stationNamesAfterCreation = 지하철_역_전체_조회();
 
-        assertThat(stationNamesAfterCreation.size()).isEqualTo(createdStationNameList.size());
-        assertThat(stationNamesAfterCreation.size()).isEqualTo(createdStationIdList.size());
-        assertThat(stationNamesAfterCreation).containsAll(createdStationNameList);
+        assertThat(stationNamesAfterCreation.size()).isEqualTo(2);
+        assertThat(stationNamesAfterCreation).containsAll(List.of(강남역, 역삼역));
 
         // when
-        for (int stationNameIdx = 0; stationNameIdx < createdStationIdList.size(); stationNameIdx++) {
-            지하철_역_삭제(createdStationIdList.get(stationNameIdx));
-        }
+        지하철_역_삭제(강남역_생성응답.getId());
+        지하철_역_삭제(역삼역_생성응답.getId());
 
         // then
         List<String> stationNamesAfterDeletion = 지하철_역_전체_조회();
-        assertThat(stationNamesAfterDeletion).doesNotContainAnyElementsOf(createdStationNameList);
-
+        assertThat(stationNamesAfterDeletion).doesNotContainAnyElementsOf(List.of(강남역, 역삼역));
     }
 
 }
